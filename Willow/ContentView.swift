bd6123a3196493afa6e2fb8289267f6c30603a44
Matt Gavin <mjgavin715@gmail.com>
2025-12-12 09:22:39 -0600

    Initial Commit

---
+++ Willow/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Willow
+//
+//  Created by Matthew Gavin on 12/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Willow/WillowApp.swift
@@ -0,0 +1,17 @@
+//
+//  WillowApp.swift
+//  Willow
+//
+//  Created by Matthew Gavin on 12/12/25.
+//
+
+import SwiftUI
+
+@main
+struct WillowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
